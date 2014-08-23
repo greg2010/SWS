1be57c03e0439a2e42aabf79366b4fac35c223fe
root <root@29252.local>
2014-08-23 12:20:04 +0000

    1st

--- classes/APIUserManagement.class.php
+++ classes/APIUserManagement.class.php
@@ -1,13 +1,5 @@
 <?php
 
-
-/**
- * Description of APIUserManagement
- *
- * @author greg2010
- */
-
-
 interface IAPIUserManagement {
     
     function getUserKeyMask();
@@ -15,29 +7,70 @@ interface IAPIUserManagement {
     
 }
 
-class APIUserManagement extends permissions implements IAPIUserManagement {
+class APIUserManagement implements IAPIUserManagement {
+
+    use Pheal\Pheal;
     
     protected $accessMask;
-    protected $userInfo;
     protected $allowedList;
     protected $pilotInfo;
+    protected $id;
     
     private $dbPilotInfo;
     private $apiPilotInfo;
+    private $apiKey;
     
     public function __construct($id, $accessMask = NULL) {
         parent::__construct($id);
         if ($accessMask) {
         $this->accessMask = $accessMask;
         }
+        $this->db = db::getInstance();
+        $this->permissions = new permissions($id);
+        $this->UserManagement = new APIUserManagement($id);
+    }
+
+    protected function getPilotInfo() {
+        return $this->dbPilotInfo;
     }
     
-    private function getPilotInfo() {
+    private function getDbPilotInfo() {
         //Populates $dbPilotInfo
+        try {
+            $query = "SELECT * FROM `pilotInfo` WHERE `id` = '$this->id'";
+            $result = $this->db->query($db);
+            $dbPilotInfo = $this->db->fetchArray($result);
+        } catch (Exception $ex) {
+            return $ex->getMessage();
+        }
     }
     
-    private function getApiInfo () {
+    private function getApiKey() {
+        try {
+            $query = "SELECT `keyID`, `vCode`, `characterID` FROM `apiList` WHERE `id` = '$this->id' AND `keyStatus` = '1'";
+            $result = $this->db->query($db);
+            $apiKey = $this->db->fetchRow($result);
+        } catch (Exception $ex) {
+            return $ex->getMessage();
+        }
+    }
+
+    private function getApiPilotInfo() {
         //Populates $apiPrivateInfo
+        $pheal = new Pheal($apiKey[0], $apiKey[1]);
+        try {
+            $response = $pheal->APIKeyInfo();
+            // $response->key->accessMask == 0
+            // $response->key->type == Account
+            // $this->unsetPermissions(array('webReg_Valid'))
+            for($i=0; $i<sizeof($response->key->characters); $i++){
+                if($response->key->characters[$i]->characterID === $apiKey[2]){
+                    $apiPilotInfo = $response->key->characters[$i];
+                }
+            }
+        } catch (\Pheal\Exceptions\PhealException $e) {
+            return $e->getMessage();
+        }
     }
     
     private function comparePilotInfo () {
@@ -60,3 +93,5 @@ class APIUserManagement extends permissions implements IAPIUserManagement {
         
     }
 }
+
+?>
