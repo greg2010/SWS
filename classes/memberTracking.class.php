a0c01b5160fa7f252539a8c3363030b06ff8b657
Unuk <root@29252.local>
2014-09-09 12:27:28 +0000

    silo & supercap mon egg

---
+++ classes/memberTracking.class.php
@@ -0,0 +1,27 @@
+<?php
+
+use Pheal\Pheal;
+use Pheal\Core\Config;
+
+class memberTracking {
+
+    protected $keyInfo = array();
+    private $log;
+    private $db;
+    private $poslist = array();
+
+	public function __construct($keyInfo = array()){
+        $this->db = db::getInstance();
+        $this->log = new logging();
+        $this->keyInfo = $keyInfo;
+        Config::getInstance()->cache = new \Pheal\Cache\FileStorage(dirname(__FILE__) . '/../phealcache/');
+        Config::getInstance()->access = new \Pheal\Access\StaticCheck();
+    }
+
+    public function updateSuperCapList(){
+        return $this->log->get();
+    }
+
+}
+
+?>
