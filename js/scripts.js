d741605b575775b17270a25258cd9840b79c993d
greg2010 <greg2008200@gmail.com>
2014-09-21 02:23:43 +0400

    weird bug fix

--- js/scripts.js
+++ js/scripts.js
@@ -39,47 +39,38 @@ function SendRequest(){
         });
     }
 }
-//    $(document).ready(function() {
-//        for(i=0; i<20; i++)
-//        {
-//            $('#target').append($('<input type="radio">').text(i).attr('value', i));
-////            var radioBtn = $("<input type='radio' name='rbtnCount' id='myradio"+i+"' />");
-////            radioBtn.appendTo('#target');
-////        }
-//    })
-//    document.getElementById("submit").disabled = false;
-//    };
-//$(document).ready(function() {
-//    $('#email').blur(function() {
-//        if($(this).val() !== '') {
-//            var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-//            if(pattern.test($(this).val())){
-//                $(this).css({'border' : '1px solid #569b44'});
-//                $('#email-valid').text('');
-//            } else {
-//                $(this).css({'border' : '1px solid #ff0000'});
-//                $('#email-valid').text('Your e-mail is not valid!').css({'color' : '#ff0000'});
-//            }
-//        } else {
-//            // Поле email пустое, выводим предупреждающее сообщение
-//            $(this).css({'border' : '1px solid #ff0000'});
-//            $('#email-valid').text('Please type your e-mail!').css({'color' : '#ff0000'});
-//        }
-//    });
-//        $('#password').blur(function() {
-//        if($(this).val() !== '') {
-//            var count = $(this).val().length;
-//            if(count > 6){
-//                $(this).css({'border' : '1px solid #569b44'});
-//                $('#password-valid').text('');
-//            } else {
-//                $(this).css({'border' : '1px solid #ff0000'});
-//                $('#password-valid').text('Your password must consist minimum of 6 symbols!').css({'color' : '#ff0000'});
-//            }
-//        } else {
-//            // Поле email пустое, выводим предупреждающее сообщение
-//            $(this).css({'border' : '1px solid #ff0000'});
-//            $('#password-valid').text('Password field is empty!').css({'color' : '#ff0000'});
-//        }
-//    });
-//});
+document.getElementById("submit").disabled = false;
+$(document).ready(function() {
+    $('#email').blur(function() {
+        if($(this).val() !== '') {
+            var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+            if(pattern.test($(this).val())){
+                $(this).css({'border' : '1px solid #569b44'});
+                $('#email-valid').text('');
+            } else {
+                $(this).css({'border' : '1px solid #ff0000'});
+                $('#email-valid').text('Your e-mail is not valid!').css({'color' : '#ff0000'});
+            }
+        } else {
+            // Поле email пустое, выводим предупреждающее сообщение
+            $(this).css({'border' : '1px solid #ff0000'});
+            $('#email-valid').text('Please type your e-mail!').css({'color' : '#ff0000'});
+        }
+    });
+        $('#password').blur(function() {
+        if($(this).val() !== '') {
+            var count = $(this).val().length;
+            if(count > 6){
+                $(this).css({'border' : '1px solid #569b44'});
+                $('#password-valid').text('');
+            } else {
+                $(this).css({'border' : '1px solid #ff0000'});
+                $('#password-valid').text('Your password must consist minimum of 6 symbols!').css({'color' : '#ff0000'});
+            }
+        } else {
+            // Поле email пустое, выводим предупреждающее сообщение
+            $(this).css({'border' : '1px solid #ff0000'});
+            $('#password-valid').text('Password field is empty!').css({'color' : '#ff0000'});
+        }
+    });
+});
