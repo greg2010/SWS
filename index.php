0511146976f6705f1fcc065824f00145f283ef5f
greg2010 <greg2008200@gmail.com>
2014-09-28 19:30:31 +0400

    ts status

--- index.php
+++ index.php
@@ -6,4 +6,9 @@ include 'header.php';
 $templateName = $thisPage;
 $pagePermissions = array("webReg_Valid");
 
+//$ts3 = new ts3();
+//$tsStats = $ts3->status();
+$toTemplate['ts']['status'] = $tsStats[status];
+$toTemplate['ts']['online'] = $tsStats[online];
+
 require 'twigRender.php';
