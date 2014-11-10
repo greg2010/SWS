9e98bae7b7daa7db0a14fb8962bd2e82799aa6ca
Charlie Root <root@ns3313640.ip-5-135-164.eu>
2014-11-11 00:12:23 +0300

    atap

--- classes/notif_text.class.php
+++ classes/notif_text.class.php
@@ -13,6 +13,7 @@ class notif_text {
 
 	public function __construct($text, $cid, $aid, $isf){
         $this->txtarr = yaml_parse($text);
+        //var_dump($this->txtarr);
         $this->db = db::getInstance();
         $this->log = new logging();
         $this->apiUserManagement = new APIUserManagement();
@@ -26,19 +27,20 @@ class notif_text {
             if($this->txtarr[corpID] || $this->txtarr[aggressorCorpID]) $this->CorpID();
             if($this->txtarr[allianceID] || $this->txtarr[aggressorAllianceID]) $this->AllianceID();
         }
-        if($this->txtarr[typeID]) $this->typeID();
+        if($this->txtarr[typeID]) $this->txtarr[typeName] = $this->typeID($this->txtarr[typeID]);
         if($this->txtarr[wants]) $this->wants();
-        if($this->txtarr[moonID]) $this->moonID();
+        if($this->txtarr[corpsPresent]) $this->corpsPresent();
+        if($this->txtarr[moonID]) $this->txtarr[moonName] = $this->moonID($this->txtarr[moonID]);
         if($this->txtarr[solarSystemID]) $this->solarSystemID();
-        if($this->txtarr[planetID]) $this->planetID();
+        if($this->txtarr[planetID]) $this->txtarr[planetName] = $this->moonID($this->txtarr[planetID]);
     }
 
     private function Owner($cid, $aid){
         try {
-            $this->txtarr[OwnerCorpName] = $this->apiOrgManagement->getCorporationName($id);;
-            $this->txtarr[OwnerCorpTicker] = $this->apiOrgManagement->getCorporationTicker($id);;
-            $this->txtarr[OwnerAllyName] = $this->apiOrgManagement->getAllianceName($id);;
-            $this->txtarr[OwnerAllyTicker] = $this->apiOrgManagement->getAllianceTicker($id);;
+            $this->txtarr[OwnerCorpName] = $this->apiOrgManagement->getCorporationName($cid);
+            $this->txtarr[OwnerCorpTicker] = $this->apiOrgManagement->getCorporationTicker($cid);
+            $this->txtarr[OwnerAllyName] = $this->apiOrgManagement->getAllianceName($aid);
+            $this->txtarr[OwnerAllyTicker] = $this->apiOrgManagement->getAllianceTicker($aid);
         } catch (\Pheal\Exceptions\PhealException $e) {
             $this->log->put("Owner", "err " . $ex->getMessage());
         }
@@ -72,11 +74,11 @@ class notif_text {
         }
     }
 
-    private function typeID(){
+    private function typeID($id){
         try {
-            $query = "SELECT `typeName` FROM `invTypes` WHERE `typeID`='{$this->txtarr[typeID]}' LIMIT 1";
+            $query = "SELECT `typeName` FROM `invTypes` WHERE `typeID`='$id' LIMIT 1";
             $result = $this->db->query($query);
-            $this->txtarr[typeName] = $this->db->getMysqlResult($result);
+            return $this->db->getMysqlResult($result);
         } catch (Exception $ex) {
             $this->log->put("typeID", "err " . $ex->getMessage());
         }
@@ -85,20 +87,35 @@ class notif_text {
     private function wants(){
         for($i=0; $i < count($this->txtarr[wants]); $i++){
             try {
-                $query = "SELECT `typeName` FROM `invTypes` WHERE `typeID`='{$this->txtarr[wants][$i][typeID]}' LIMIT 1";
-                $result = $this->db->query($query);
-                $this->txtarr[wants][$i][typeName] = $this->db->getMysqlResult($result);
+                $this->txtarr[wants][$i][typeName] = $this->typeID($this->txtarr[wants][$i][typeID]);
             } catch (Exception $ex) {
                 $this->log->put("wants " . $i, "err " . $ex->getMessage());
             }
         }
     }
 
-    private function moonID(){
+    private function corpsPresent(){
+        for($i=0; $i < count($this->txtarr[corpsPresent]); $i++){
+            try {
+                $this->txtarr[corpsPresent][$i][allyName] = $this->apiOrgManagement->getAllianceName($this->txtarr[corpsPresent][$i][allianceID]);
+                $this->txtarr[corpsPresent][$i][allyTicker] = $this->apiOrgManagement->getAllianceTicker($this->txtarr[corpsPresent][$i][allianceID]);
+                $this->txtarr[corpsPresent][$i][corpName] = $this->apiOrgManagement->getCorporationName($this->txtarr[corpsPresent][$i][corpID]);
+                $this->txtarr[corpsPresent][$i][corpTicker] = $this->apiOrgManagement->getCorporationTicker($this->txtarr[corpsPresent][$i][corpID]);
+                for($j=0; $j < count($this->txtarr[corpsPresent][$i][towers]); $j++){
+                    $this->txtarr[corpsPresent][$i][towers][$j][moonName] = $this->moonID($this->txtarr[corpsPresent][$i][towers][$j][moonID]);
+                    $this->txtarr[corpsPresent][$i][towers][$j][typeName] = $this->typeID($this->txtarr[corpsPresent][$i][towers][$j][typeID]);
+                }
+            } catch (Exception $ex) {
+                $this->log->put("corpsPresent " . $i, "err " . $ex->getMessage());
+            }
+        }
+    }
+
+    private function moonID($id){
         try {
-            $query = "SELECT `itemName` FROM `mapDenormalize` WHERE `itemID`='{$this->txtarr[moonID]}' LIMIT 1";
+            $query = "SELECT `itemName` FROM `mapDenormalize` WHERE `itemID`='$id' LIMIT 1";
             $result = $this->db->query($query);
-            $this->txtarr[moonName] = $this->db->getMysqlResult($result);
+            return $this->db->getMysqlResult($result);
         } catch (Exception $ex) {
             $this->log->put("moonID", "err " . $ex->getMessage());
         }
@@ -111,16 +128,6 @@ class notif_text {
             $this->txtarr[solarSystemName] = $this->db->getMysqlResult($result);
         } catch (Exception $ex) {
             $this->log->put("solarSystemID", "err " . $ex->getMessage());
-        }
-    }
-
-    private function planetID(){
-        try {
-            $query = "SELECT `itemName` FROM `mapDenormalize` WHERE `itemID`='{$this->txtarr[planetID]}' LIMIT 1";
-            $result = $this->db->query($query);
-            $this->txtarr[planetName] = $this->db->getMysqlResult($result);
-        } catch (Exception $ex) {
-            $this->log->put("planetID", "err " . $ex->getMessage());
         }
     }
 
