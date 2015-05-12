f473f74f73d925ee9500f81e63f4bf9c1d0840cc
root <root@ns3267166.ip-5-39-80.eu>
2015-05-12 20:48:11 +0200

    wh update

--- classes/wormholes.class.php
+++ classes/wormholes.class.php
@@ -11,36 +11,73 @@ class wormholes{
     }
 
     private function getUserName($id){
-        return "fofofo";
+        $query = "SELECT `login` FROM `users` WHERE `id`='$id'";
+        $result = $this->db->query($query);
+        return $this->db->hasRows($result);
     }
 
     private function getType($name){
-        $type = array(
-            "Name" => $name,
-            "Life" => "24",
-            "Leads To" => "high",
-            "Max Mass" => "22222",
-            "Max Jumpable" => "1111"
-        );
+        $query = "SELECT `dgmTypeAttributes`.`attributeID`, `dgmTypeAttributes`.`valueInt`, `dgmTypeAttributes`.`valueFloat` FROM `dgmTypeAttributes` INNER JOIN `invTypes` ON `dgmTypeAttributes`.`typeID` = `invTypes`.`typeID` WHERE `invTypes`.`typeName` = 'Wormhole $name'";
+        $result = $this->db->query($query);
+        $tmparr = $this->db->fetchAssoc($result);
+        $type = array();
+        foreach ($tmparr as $tmp){
+            if($tmp[attributeID] == "1381"){
+                switch($tmp[valueInt]){
+                    case 1: $type["Leads To"] = "Class 1"; break;
+                    case 2: $type["Leads To"] = "Class 2"; break;
+                    case 3: $type["Leads To"] = "Class 3"; break;
+                    case 4: $type["Leads To"] = "Class 4"; break;
+                    case 5: $type["Leads To"] = "Class 5"; break;
+                    case 6: $type["Leads To"] = "Class 6"; break;
+                    case 7: $type["Leads To"] = "High-Sec"; break;
+                    case 8: $type["Leads To"] = "Low-Sec"; break;
+                    case 9: $type["Leads To"] = "Null-Sec"; break;
+                }
+            }
+            if($tmp[attributeID] == "1382"){
+                $type["Life"] = $tmp[valueInt]/60;
+            }
+            if($tmp[attributeID] == "1383"){
+                $type["Max Mass"] = ($tmp[valueInt]) ? $tmp[valueInt] : $tmp[valueFloat];
+            }
+            /*if($tmp[attributeID] == "1384"){
+                $type["Mass Regeneration"] = ($tmp[valueInt]) ? $tmp[valueInt] : $tmp[valueFloat];
+            }*/
+            if($tmp[attributeID] == "1385"){
+                $type["Max Jumpable"] = ($tmp[valueInt]) ? $tmp[valueInt] : $tmp[valueFloat];
+            }
+            /*if($tmp[attributeID] == "1457"){
+                $type["Target Distribution ID"] = $tmp[valueInt];
+            }*/
+        }
         return $type;
     }
 
     private function getSystemInfo($id){
-        //проверка на вх
-        //если вх
-        $system = array(
-            "Name" => "J123",
-            "Wormhole Class" => "c7",
-            "Static" => "u210",
-            "System Effect" => "none"
-        );
-        //если не вх
-        $system = array(
-            "Name" => "ABC",
-            "Region" => "DEF",
-            "Constellation" => "GHI",
-            "Security Level" => "0"
-        );
+        $query = "SELECT `regionID`, `constellationID`, `solarSystemName`, `security` FROM `mapSolarSystems` WHERE `solarSystemID`='$id'";
+        $result = $this->db->query($query);
+        $tmparr = $this->db->fetchAssoc($result);
+        $system = array();
+        $system["Name"] = $tmparr["solarSystemName"];
+        $query = "SELECT `class`, `static1`, `static1` FROM `mapWHConstellations` WHERE `id`='{$tmparr[constellationID]}'";
+        $result = $this->db->query($query);
+        if($this->db->hasRows($result)){
+            $tmparr2 = $this->db->fetchAssoc($result);
+            $system["Wormhole Class"] = $tmparr2["class"];
+            $system["Static1"] = $this->getType($tmparr2["static1"]);
+            if($tmparr2["static2"]) $system["Static2"] = $this->getType($tmparr2["static2"]);
+            $query = "SELECT `invTypes`.`typeName` FROM `mapDenormalize` LEFT JOIN `invTypes` ON `mapDenormalize`.`typeid` = `invTypes`.`typeID` WHERE `mapDenormalize`.`solarSystemID` = '$id' AND `mapDenormalize`.`groupID` = '995'";
+            $result = $this->db->query($query);
+            if($this->db->hasRows($result)) $type["System Effect"] = $this->db->getMysqlResult($result);
+        } else{
+            $query = "SELECT `itemName` FROM `mapDenormalize` WHERE `itemID` = '{$tmparr[regionID]}' OR `itemID` = '{$tmparr[constellationID]}'";
+            $result = $this->db->query($query);
+            $tmparr2 = $this->db->fetchAssoc($result);
+            $system["Region"] = $tmparr2[0][itemName];
+            $system["Constellation"] = $tmparr2[1][itemName];
+            $system["Security Level"] = round($tmparr[security], 1);
+        }
         return $system;
     }
 
@@ -50,8 +87,17 @@ class wormholes{
     }
 
     private function getSystemID($name){
-        return 0;
-        //и проверку на реальность системы
+        $query = "SELECT `solarSystemID` FROM `mapSolarSystems` WHERE `solarSystemName`='$name' LIMIT 1";
+        $result = $this->db->query($query);
+        return ($this->db->hasRows($result)) ? $this->db->getMysqlResult($result) : 0;
+    }
+
+    private function checkWH($name){
+        $query = "SELECT `typeName` FROM `invTypes` WHERE `typeName` REGEXP '^Wormhole [A-Z]{1}[0-9]{3}$'";
+        $result = $this->db->query($query);
+        $tmparr = $this->db->fetchRow($result);
+        foreach ($tmparr as $tmp) if($tmp[0] == "Wormhole $name") return true;
+        return false;
     }
 
     public function getWHList(){
@@ -89,11 +135,13 @@ class wormholes{
     }
 
     public function updateWH($wh_id = NULL, $ID, $Type, $System, $Leads, $Life, $Mass){
-        //проверка что $ID состоит из цифр иначе возвращает некий код ошибки
-        //проверка на реальность $Type иначе возвращает некий код ошибки
-        $date = date("Y-m-d H:i:s");
+        if(!preg_match('/^\d{3}$/', $ID)) throw new Exception("Invalid signature id!", 31);
+        if(!$this->checkWH($Type)) throw new Exception("Invalid wormhole type!", 32);
         $intSystem = $this->getSystemID($System);
+        if($intSystem == 0) throw new Exception("Invalid system name!", 33);
         $intLeads = $this->getSystemID($Leads);
+        if($intLeads == 0) throw new Exception("Invalid target system name!", 34);
+        $date = date("Y-m-d H:i:s");
         $intLife = ($Life == "Critical") ? 0 : 1;
         if($Mass == "Critical") $intMass = 0;
         elseif($Mass == "Destab") $intMass = 1;
--- updateWH.php
+++ updateWH.php
@@ -7,7 +7,17 @@ try{
 	$wormholes = new wormholes($_GET[userID]);
 	$wormholes->updateWH($_GET[wh_id], $_GET[ID], $_GET[Type], $_GET[System], $_GET[Leads], $_GET[Life], $_GET[Mass]);
 } catch (Exception $ex){
-    //13,30
+	if ($ex->getCode() == 31){
+        $toTemplate["errorMsg"] = "Invalid signature id!";
+    } elseif ($ex->getCode() == 32){
+        $toTemplate["errorMsg"] = "Invalid wormhole type!";
+    } elseif ($ex->getCode() == 33){
+        $toTemplate["errorMsg"] = "Invalid system name!";
+    } elseif ($ex->getCode() == 34){
+        $toTemplate["errorMsg"] = "Invalid target system name!";
+    } else{
+        $toTemplate["errorMsg"] = "Database error. Try again later.";
+    }
 }
 header("Location: /wh.php"); //redirect
 
